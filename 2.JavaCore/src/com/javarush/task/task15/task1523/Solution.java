eed283135abb4d6662fc09a9bd59b1af847431b5
Tigran <tigranma@gmail.com>
2020-10-06 18:56:42 +0300

    Final init commit

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    Solution(){
+
+    }
+    protected Solution(int a){
+
+    }
+    private Solution (double b){
+
+    }
+    public Solution (String s){
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(int c){
+
+    }
+    public SubSolution(String x){
+
+    }
+    protected SubSolution(double n){
+
+    }
+}
