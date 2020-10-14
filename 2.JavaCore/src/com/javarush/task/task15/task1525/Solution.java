7a8c51b67a1f8e8ea6b603ea8fd47e572578ed67
Tigran <tigranma@gmail.com>
2020-10-14 22:30:55 +0300

    Second init commit

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task15.task1525;
 
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
