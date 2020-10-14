dd703f7437d504fe9bdc1957045831d7a2c6f91a
Tigran <tigranma@gmail.com>
2020-10-14 22:28:44 +0300

    Second init commit

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -1,5 +1,5 @@
 package com.javarush.task.task15.task1525;
 
 public class Statics {
-    public static String FILE_NAME = "/Users/anigukasan/Desktop";
+    public static String FILE_NAME = "/Users/anigukasan/Desktop/FILE_NAME.txt";
 }
