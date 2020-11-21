d2a15067adcd1ba3a236e998e02d737e9ae11ce0
Tigran <tigranma@gmail.com>
2020-11-21 14:30:12 +0300

    Final init commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<String>();
+        set.add("Лама");
+        set.add("Лебедь");
+        set.add("Лось");
+        set.add("Лиса");
+        set.add("Лев");
+        set.add("Лемур");
+        set.add("Леопард");
+        set.add("Лопух");
+        set.add("Лист");
+        set.add("Лиана");
+        set.add("Лодыжка");
+        set.add("Лень");
+        set.add("Ловкач");
+        set.add("Листва");
+        set.add("Лес");
+        set.add("Лоцман");
+        set.add("Лагуна");
+        set.add("Лед");
+        set.add("Ледокол");
+        set.add("Лексика");
+        return set;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
