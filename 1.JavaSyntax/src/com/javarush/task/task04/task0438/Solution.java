4a66d7e7c6072083ab09857a57e2db64a8722cc5
Tigran <tigranma@gmail.com>
2021-01-07 15:40:51 +0300

    Final init commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0438;
+
+/* 
+Рисуем линии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for(int i=0; i<11; i++){
+                if(i<=0){
+                for(int j=1; j<10; j++){
+                System.out.print(8);
+            }
+                }
+            System.out.println(8);
+        }
+    }
+}
