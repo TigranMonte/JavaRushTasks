72717c07adc3d6fe5a2d2db36fd6d0c893ff8f4a
Tigran <tigranma@gmail.com>
2021-01-11 21:34:43 +0300

    Final init commit

--- 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task03.task0305;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println ("JULY " + "6 " + "1988" );//напишите тут ваш код
+        System.out.println ("JULY " + "6 " + "1988" );
     }
 }
