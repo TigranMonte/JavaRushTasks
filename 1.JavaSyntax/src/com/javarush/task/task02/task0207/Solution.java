dd667403c5bf998d0b1cacbbe61951591c439b74
Tigran <tigranma@gmail.com>
2021-01-19 22:27:56 +0300

    Final init commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task02.task0207;
+
+/* 
+Кусочек калькулятора
+*/
+public class Solution {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 10;
+        System.out.println(a + b);
+        System.out.println(a * b);
+    }
+}
