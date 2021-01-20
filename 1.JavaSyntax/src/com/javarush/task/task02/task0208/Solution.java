3819f81d865b102c0fb79511596f7e1852bda102
Tigran <tigranma@gmail.com>
2021-01-20 22:23:48 +0300

    Final init commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0208/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task02.task0208;
+
+/* 
+Одного кота нам мало
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Cat kiska = new Cat();
+        Cat vasilek = new Cat();
+    }
+    public static class Cat {
+
+    }
+}
