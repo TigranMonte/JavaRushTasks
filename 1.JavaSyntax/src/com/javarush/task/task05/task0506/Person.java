667e6db9538e94e3cbb9a5c346b025cd96899e84
Tigran <tigranma@gmail.com>
2021-01-27 23:32:16 +0300

    Class Person

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0506;
+
+/* 
+Человечки
+*/
+
+public class Person {
+    String name = "Tiko";
+    int age = 31;
+    String address = "Krasnodar, Garazhnaya st. 107";
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
