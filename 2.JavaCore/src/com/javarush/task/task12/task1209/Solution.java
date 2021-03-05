bf2e8ecb34411ff6c9f5eb19973f7100f906469b
Tigran <tigranma@gmail.com>
2021-03-05 22:46:30 +0300

    Method Math.min

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b){
+        return Math.min(a, b);
+    }
+    public static long min(long c, long z){
+        return Math.min(c, z);
+    }
+    public static double min(double x, double y){
+        return Math.min(x, y);
+    }
+}
