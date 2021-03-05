923a809e7d70e45b238d9e603851b62e992f51aa
Tigran <tigranma@gmail.com>
2021-03-05 22:47:18 +0300

    Method Math.max

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int a, int b){
+        return Math.max(a, b);
+    }
+    public static long max(long c, long z){
+        return Math.max(c, z);
+    }
+    public static double max(double x, double y){
+        return Math.max(x, y);
+    }
+}
