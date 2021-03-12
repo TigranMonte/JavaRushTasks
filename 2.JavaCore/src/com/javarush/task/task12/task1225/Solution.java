72308cfac22f551979b19c5e7d4f4d9678528b0e
Tigran <tigranma@gmail.com>
2021-03-12 23:36:29 +0300

    Final init commit

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1225/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task12.task1225;
+
+/* 
+Посетители
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Animal()));
+    }
+
+    public static String getObjectType(Object o) {
+        if (o instanceof Lion){
+            return "Лев";
+        }
+        if (o instanceof Tiger){
+            return "Тигр";
+        }
+        if (o instanceof Cat){
+            return "Кот";
+        }
+        if (o instanceof Bull) return "Бык";
+        if (o instanceof Cow) return "Корова";
+       else return "Животное";
+    }
+
+    public static class Cat extends Animal   //<--Классы наследуются!!
+    {
+    }
+
+    public static class Tiger extends Cat {
+    }
+
+    public static class Lion extends Cat {
+    }
+
+    public static class Bull extends Animal {
+    }
+
+    public static class Cow extends Animal {
+    }
+
+    public static class Animal {
+    }
+}
+
+//1. Класс Solution должен содержать классы Cat, Tiger, Lion, Bull, Cow, Animal.
+//2. Класс Solution должен содержать метод String getObjectType(Object o).
+//3. Метод getObjectType() должен возвращать строку "Кот", если передан объект типа Cat.
+//4. Метод getObjectType() должен возвращать строку "Тигр", если передан объект типа Tiger.
+//5. Метод getObjectType() должен возвращать строку "Лев", если передан объект типа Lion.
+//6. Метод getObjectType() должен возвращать строку "Бык", если передан объект типа Bull.
+//7. Метод getObjectType() должен возвращать строку "Корова", если передан объект типа Cow.
+//8. Метод getObjectType() должен возвращать строку "Животное", если передан объект типа Animal.
+//9. Программа должна выводить на экран результат метода getObjectType().
