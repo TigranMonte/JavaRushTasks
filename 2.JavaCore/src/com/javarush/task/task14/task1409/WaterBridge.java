829b0b58568f7ca82248715c04907d1d07661a2e
Tigran <tigranma@gmail.com>
2021-04-01 23:40:37 +0300

    Интерфейс Bridge для подсчета машин

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/WaterBridge.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task14.task1409;
+
+public class WaterBridge implements Bridge{
+    @Override
+    public int getCarsCount() {
+        return 100;
+    }
+    public static void println(Bridge bridge) {
+
+    }
+}
+
