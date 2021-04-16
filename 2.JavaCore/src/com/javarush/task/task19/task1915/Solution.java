271ffd5c9d14029ea36583f2c10b1a718fe529c3
Tigran <tigranma@gmail.com>
2021-04-16 19:13:44 +0300

    Ридер обертка дублирует текст и на консоль и в файл

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task19.task1915;
+
+import java.io.*;
+
+/* 
+Дублируем текст
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        reader.close();
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String result = outputStream.toString();
+        FileOutputStream fileOutputStream = new FileOutputStream(s);
+        fileOutputStream.write(outputStream.toByteArray());
+
+        System.out.println(result);
+        fileOutputStream.close();
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
