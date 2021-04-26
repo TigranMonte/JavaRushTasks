3dae26382bcae46553af08b0df1c222a7a5c3f03
Tigran <tigranma@gmail.com>
2021-04-26 20:33:34 +0300

    Сериализация

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3706/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task37.task3706;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Давно забытый Array
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Number> numbers = Arrays.asList(1, 2, 3);
+        addDataToList(numbers, getData());
+        System.out.println(numbers);
+    }
+
+    public static Number[] getData() {
+        return new Number[]{};
+    }
+
+    public static void addDataToList(List<Number> list, Number... data) {
+        for (Number number : data) {
+            list.add(number);
+        }
+    }
+}
