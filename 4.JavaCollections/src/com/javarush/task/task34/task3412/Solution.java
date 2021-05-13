913345c2c3fe38a82c5911c475472a5528508e6d
Tigran <tigranma@gmail.com>
2021-05-14 01:23:02 +0300

    Работа с датами

--- 4.JavaCollections/src/com/javarush/task/task34/task3412/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3412/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task34.task3412;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 
 import java.util.Date;
 
