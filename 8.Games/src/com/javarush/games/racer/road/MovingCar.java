41bc62a32435312c313095dfd63f40fb5abdfc1f
Tigran <tigranma@gmail.com>
2021-05-28 23:44:25 +0300

    Racer game

--- 8.Games/src/com/javarush/games/racer/road/MovingCar.java
+++ 8.Games/src/com/javarush/games/racer/road/MovingCar.java
@@ -9,29 +9,23 @@ public class MovingCar extends RoadObject {
         super(RoadObjectType.DRUNK_CAR, x, y);
         speed = 1;
     }
-
     @Override
     public void move(int boost, List<RoadObject> roadObjects) {
         y += boost;
         x += dx;
-
         if (y > 0 && dx == 0) {
             dx = 1;
         }
-
         if (x < RoadManager.LEFT_BORDER + 1) {
             dx = 1;
         }
-
         if (x + width > RoadManager.RIGHT_BORDER - 1) {
             dx = -1;
         }
-
         if (checkRoadHit(roadObjects)) {
             dx = -dx;
         }
     }
-
     private boolean checkRoadHit(List<RoadObject> items) {
         for (RoadObject item : items) {
             if (item == this || item.type == RoadObjectType.THORN) {
